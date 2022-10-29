da167b1e14bd58c96d6b6aecd679f8088391fdac
Shaheer Khan <shaheerkhan@Shaheers-MacBook-Pro.local>
2022-10-29 18:24:18 -0500

    Added avaliable tables

--- Backend/routes/controller.js
+++ Backend/routes/controller.js
@@ -1,64 +1,61 @@
 const router = require('express').Router();
 
 /* Create the Database Connection */
-var pg = require('pg');
 var conString = "postgres://bbxgyxcu:o6sN_sfzjG5uNdqtK3yxaucHX3SDXpFa@heffalump.db.elephantsql.com/bbxgyxcu" //Can be found in the Details page
-var client = new pg.Client(conString);
+const pgp = require("pg-promise")();
+const db = pgp(conString);
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-/* User Table */
-router.post('/User', (req, res) => {
+/* Customer Table */
+router.post('/User', async (req, res) => {
   const requestBody = req.body;
-  client.connect((err) => {
-    if(err) {
-      return console.error("[Connection Error]: " + err);
-    };
+  bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+    await db.query(
+      `INSERT INTO customer(Username, Password, Name, MailingAddress, BillingAddress, EarnedPoints, PaymentMethod) 
+      VALUES(
+        '${requestBody.username}',
+        '${hash}',
+        '${requestBody.name}',
+        '${requestBody.mailingAddress}',
+        '${requestBody.billingAddress}',
+        '${0}',
+        '${requestBody.paymentMethod}'
+      )`
+    );
+  });
 
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-      client.query(
-        `INSERT INTO customer(Username, Password, Name, MailingAddress, BillingAddress, EarnedPoints, PaymentMethod) 
-        VALUES(
-          '${requestBody.username}',
-          '${hash}',
-          '${requestBody.name}',
-          '${requestBody.mailingAddress}',
-          '${requestBody.billingAddress}',
-          '${0}',
-          '${requestBody.paymentMethod}'
-        )`, 
-      (err, result) => {
-        if(err) {
-          return console.error("[Query Error]: ", err)
-        }
-        client.end();
-      });
-    });
-  })
+  
 })
 
-/* Table Table */
+/* ResturantTable Table */
+router.get('/ResturantTable/:tableId/:date', async (req, res) => {
+    req.params.date = "2022-10-29";
+    const tables = await db.query(`SELECT * FROM ResturantTable`);
+    
+    const avaliableTables = [];
+    for(const table of tables) {
+      const count = await db.query(
+        `SELECT Count(Id) FROM reservation 
+        WHERE resturanttableid = '${table.id}' AND date = '${req.params.date}'`
+      );
+      
+      const reservationExists = parseInt(count[0].count);
+      if(!reservationExists) {
+        avaliableTables.push(table.id);
+      }
+    }
+
+    res.send(avaliableTables);
+})
 
 /* Reservation Table */
 
 /* TestConnection Table */
-router.get('/TestConnection', (req, res) => {
-  client.connect((err) => {
-    if(err) {
-      return console.error("[Connection Error]: " + err);
-    };
-
-    client.query(`SELECT * FROM TestConnection`, (err, result) => {
-      if(err) {
-        return console.error("[Query Error]: ", err)
-      }
-
-      const data = result.rows;
-      res.send(data);
-      client.end();
-    });
-  })
+router.get('/TestConnection', async (req, res) => {
+  const result = await db.query(`SELECT * FROM TestConnection`);
+  res.send(result);
 });
 
 module.exports = router;
